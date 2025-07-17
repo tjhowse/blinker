8446add520cb947d7d9c4f4e29c285ea2b37c0fc
Travis Howse <tjhowse@gmail.com>
2025-07-17 15:54:47 +1000

    WIP

--- code/blinker/blinker.ino
+++ code/blinker/blinker.ino
@@ -1,56 +1,76 @@
-// http://www.technoblogy.com/show?1YQY
+#define ws2812_port B     // Data port
+#define ws2812_pin  2     // Data out pin
 
-uint32_t counter = 0;
 
-#include <avr/wdt.h>
-#include <avr/sleep.h>
-#include "sequence.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-#define LED_OFF PORTB = 0b0100
-#define LED_ON PORTB = 0b0000
+#include "attiny10_ws2812.h"  // Include the ws2812 library
+#include "tomthumb_5x3_font.h" // Include the font header
 
-void setup() {
-  // Power saving measures:
-  // Disable ADC:
-  ADCSRA = 0;
-  // Shut down ADC and timer0
-  PRR = 0b11;
+const char message[] = "Hello World!"; // Message to display
+// const uint8_t ledOff[] = {128,128, 128}; // RGB values for LED off
+// const uint8_t ledOn[] = {0, 0, 0}; // RGB values for LED on
+// const uint8_t ledRed[] = {0, 0, 0}; // RGB values for LED red
 
-  // Setup IO:
-  DDRB = 0b0100; // PB2 as an output
-  LED_OFF;
+const cRGB ledOff = {0,0,0};
+const cRGB ledOn = {16,16,16};
+const cRGB ledRed = {0,16,0};
 
-  // Enable interrupts
-  sei();
-}
+int main(void)
+{
 
-void sleep() {
-  // Tell the watchdog timer to wake us up in 120ms.
-  wdt_enable(WDTO_120MS);
-  WDTCSR |= (1<<WDIE | 1<<WDE);
+#ifdef __AVR_ATtiny10__
+  CCP=0xD8;   // configuration change protection, write signature
+  CLKPSR=0;   // set cpu clock prescaler =1 (8Mhz) (attiny 4/5/9/10)
 
-  // Go to sleep.
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sleep_enable();
-  sleep_cpu();
-}
+#else
+  CLKPR=_BV(CLKPCE);
+  CLKPR=0;      // set clock prescaler to 1 (attiny 25/45/85/24/44/84/13/13A)
 
-// This looks at the next bit in the sequence and sets the LED accordingly.
-void blink() {
-  if (sequence[counter>>3] & (0x01 << counter%8)) {
-    LED_ON;
-  } else {
-    LED_OFF;
+#endif
+  DDRB|=_BV(ws2812_pin);
+
+  for (uint8_t i = 0; message[i] != '\0'; i++) {
+  // while (1) {
+    // uint16_t bitmap = TomThumbBitmaps[message[i] - 34];
+    // uint16_t bitmap = TomThumbBitmaps[33][1] + TomThumbBitmaps[33][0] << 8;
+    // uint16_t bitmap = (0x57 << 8) + 0xDA;
+    uint16_t bitmap = 0XD75C; // B
+    for (uint8_t column = 2; column <= 2 && column >= 0; column--) {
+      for (uint8_t row = 4; row <= 4 && row >= 0; row--) {
+        if ((bitmap >> (15-(column + row*3))) & 0x1) {
+          ws2812_sendarray((uint8_t *)&ledOn,3);
+        } else {
+          // ws2812_sendarray((uint8_t *)&ledOn,3);
+          ws2812_sendarray((uint8_t *)&ledOff,3);
+        }
+      }
+    }
+    // _delay_ms(50);
+    _delay_ms(500);
   }
-  counter = (counter + 1)%(sizeof(sequence)*8);
-}
 
-// This fires when the watchdog timer expires and wakes up the CPU.
-ISR(WDT_vect) {
-  sleep_disable();
-}
 
-void loop() {
-  blink();
-  sleep();
+  // uint8_t pos=0;
+  // uint8_t direction=1;
+  // uint8_t i;
+
+  // while(1)
+  //   {
+
+  //   for (i=0; i<pos; i++)
+  //     ws2812_sendarray((uint8_t *)&ledRed,3);     // Repeatedly send "red" to the led string.
+  //                             // No more than 1-2 s should pass between calls
+  //                             // to avoid issuing a reset condition.
+  //   for (i=0; i<(30-pos); i++)
+  //     ws2812_sendarray((uint8_t *)&ledOn,3);     // white
+
+
+  //   _delay_ms(50);                    // Issue reset and wait for 50 ms.
+
+  //   pos+=direction;
+  //   if ((pos==16)||(pos==0)) direction=-direction;
+  //   }
 }
