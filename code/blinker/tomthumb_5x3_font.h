16d1edf6f4cd51b39f0e746705292d46b7ac6455
Travis Howse <tjhowse@gmail.com>
2025-07-20 17:32:11 +1000

    Making progress

--- code/blinker/tomthumb_5x3_font.h
+++ code/blinker/tomthumb_5x3_font.h
@@ -52,36 +52,23 @@
 ** Andreas Merkle (web@blue-andi.de)
 */
 
-#define TOMTHUMB_USE_EXTENDED 0
-
-
-typedef struct {
-  uint16_t bitmapOffset; // Where this character's image starts in the bitmap
-  uint8_t  width;        // Width of the glyph in pixels
-//   uint8_t  height;       // Height of the glyph in pixels
-//   uint8_t  xAdvance;     // How far to move the cursor for the next character
-//   int8_t   xOffset;      // Horizontal offset from cursor to where to start drawing
-//   int8_t   yOffset;      // Vertical offset from baseline to top of glyph
-} GFXglyph;
-
-
 
 const uint8_t TomThumbBitmaps[] PROGMEM = {
-    0x00,             /* 0x20 space */
-    0xE8,             /* 0x21 exclam */
-    0xB4,             /* 0x22 quotedbl */
+    0x00, 0x00,       /* 0x20 space */
+    0xE8, 0x00,       /* 0x21 exclam */
+    0xB4, 0x00,       /* 0x22 quotedbl */
     0xBE, 0xFA,       /* 0x23 numbersign */
     0x79, 0xE4,       /* 0x24 dollar */
     0x85, 0x42,       /* 0x25 percent */
     0xDB, 0xD6,       /* 0x26 ampersand */
-    0xC0,             /* 0x27 quotesingle */
+    0xC0, 0x00,       /* 0x27 quotesingle */
     0x6A, 0x40,       /* 0x28 parenleft */
     0x95, 0x80,       /* 0x29 parenright */
     0xAA, 0x80,       /* 0x2A asterisk */
     0x5D, 0x00,       /* 0x2B plus */
-    0x60,             /* 0x2C comma */
-    0xE0,             /* 0x2D hyphen */
-    0x80,             /* 0x2E period */
+    0x60, 0x00,       /* 0x2C comma */
+    0xE0, 0x00,       /* 0x2D hyphen */
+    0x80, 0x00,       /* 0x2E period */
     0x25, 0x48,       /* 0x2F slash */
     0x76, 0xDC,       /* 0x30 zero */
     0x75, 0x40,       /* 0x31 one */
@@ -93,8 +80,8 @@ const uint8_t TomThumbBitmaps[] PROGMEM = {
     0xE5, 0x48,       /* 0x37 seven */
     0xF7, 0xDE,       /* 0x38 eight */
     0xF7, 0x9C,       /* 0x39 nine */
-    0xA0,             /* 0x3A colon */
-    0x46,             /* 0x3B semicolon */
+    0xA0, 0x00,       /* 0x3A colon */
+    0x46, 0x00,       /* 0x3B semicolon */
     0x2A, 0x22,       /* 0x3C less */
     0xE3, 0x80,       /* 0x3D equal */
     0x88, 0xA8,       /* 0x3E greater */
@@ -126,76 +113,4 @@ const uint8_t TomThumbBitmaps[] PROGMEM = {
     0xB5, 0x5A,       /* 0x58 X */
     0xB5, 0x24,       /* 0x59 Y */
     0xE5, 0x4E,       /* 0x5A Z */
-    0xF2, 0x4E,       /* 0x5B bracketleft */
-    0x88, 0x80,       /* 0x5C backslash */
-    0xE4, 0x9E,       /* 0x5D bracketright */
-    0x54,             /* 0x5E asciicircum */
-    0xE0,             /* 0x5F underscore */
-    0x90,             /* 0x60 grave */
 };
-
-/* {offset, width, height, advance cursor, x offset, y offset} */
-const GFXglyph TomThumbGlyphs[] PROGMEM = {
-    {0, 1},   /* 0x20 space */
-    {1, 1},   /* 0x21 exclam */
-    {2, 3},   /* 0x22 quotedbl */
-    {3, 3},   /* 0x23 numbersign */
-    {5, 3},   /* 0x24 dollar */
-    {7, 3},   /* 0x25 percent */
-    {9, 3},   /* 0x26 ampersand */
-    {11, 1},  /* 0x27 quotesingle */
-    {12, 2},  /* 0x28 parenleft */
-    {14, 2},  /* 0x29 parenright */
-    {16, 3},  /* 0x2A asterisk */
-    {18, 3},  /* 0x2B plus */
-    {20, 2},  /* 0x2C comma */
-    {21, 3},  /* 0x2D hyphen */
-    {22, 1},  /* 0x2E period */
-    {23, 3},  /* 0x2F slash */
-    {25, 3},  /* 0x30 zero */
-    {27, 2},  /* 0x31 one */
-    {29, 3},  /* 0x32 two */
-    {31, 3},  /* 0x33 three */
-    {33, 3},  /* 0x34 four */
-    {35, 3},  /* 0x35 five */
-    {37, 3},  /* 0x36 six */
-    {39, 3},  /* 0x37 seven */
-    {41, 3},  /* 0x38 eight */
-    {43, 3},  /* 0x39 nine */
-    {45, 1},  /* 0x3A colon */
-    {46, 2},  /* 0x3B semicolon */
-    {47, 3},  /* 0x3C less */
-    {49, 3},  /* 0x3D equal */
-    {51, 3},  /* 0x3E greater */
-    {53, 3},  /* 0x3F question */
-    {55, 3},  /* 0x40 at */
-    {57, 3},  /* 0x41 A */
-    {59, 3},  /* 0x42 B */
-    {61, 3},  /* 0x43 C */
-    {63, 3},  /* 0x44 D */
-    {65, 3},  /* 0x45 E */
-    {67, 3},  /* 0x46 F */
-    {69, 3},  /* 0x47 G */
-    {71, 3},  /* 0x48 H */
-    {73, 3},  /* 0x49 I */
-    {75, 3},  /* 0x4A J */
-    {77, 3},  /* 0x4B K */
-    {79, 3},  /* 0x4C L */
-    {81, 3},  /* 0x4D M */
-    {83, 3},  /* 0x4E N */
-    {85, 3},  /* 0x4F O */
-    {87, 3},  /* 0x50 P */
-    {89, 3},  /* 0x51 Q */
-    {91, 3},  /* 0x52 R */
-    {93, 3},  /* 0x53 S */
-    {95, 3},  /* 0x54 T */
-    {97, 3},  /* 0x55 U */
-    {99, 3},  /* 0x56 V */
-    {101, 3}, /* 0x57 W */
-    {103, 3}, /* 0x58 X */
-    {105, 3}, /* 0x59 Y */
-    {107, 3}, /* 0x5A Z */
-};
-
-// const GFXfont TomThumb PROGMEM = {(uint8_t *)TomThumbBitmaps,
-//                                   (GFXglyph *)TomThumbGlyphs, 0x20, 0x7E, 6};
--- code/generate_6_bit_compacted_hex.py
+++ code/generate_6_bit_compacted_hex.py
@@ -2,32 +2,44 @@
 
 # This program accepts a string as its first argument.
 # It converts the sting from 8-bit ASCII to 6-bit by
-# subtracting 46, then discarding the last 2 bits.
+# subtracting 32, then discarding the two MSBs.
 # It prints the result in one-byte hexadecimal format.
 # It compacts the bits together so there is no padding between characters.
 # I.E.
 # "A.B." becomes:
-# 010011 000000 010100 000000
-# 010011000000010100000000
-# 4C0500
+# 100001 001110 100010 001110
+# 10000100 11101000 10001110
+# 84E88E
+# 0x84, 0xE8, 0x8E
+
 
 def convert_to_6bit_packed_string(input_string) -> str:
     packed_bits = []
     for char in input_string:
-        if char == ' ':
-            packed_bits.append('000000')
-        else:
-            value = ord(char) - 46
-            if value < 0 or value > 63:
-                raise ValueError(f"Character '{char}' is out of range for 6-bit encoding.")
-            packed_bits.append(f"{value:06b}")
+        value = ord(char) - 32
+        if value < 0 or value > 63:
+            raise ValueError(f"Character '{char}' is out of range for 6-bit encoding.")
+        packed_bits = [f"{value:06b}"] + packed_bits
     # Join all the packed bits and convert to hex
     packed_bits_str = ''.join(packed_bits)
+    # Pad the packed bits to make sure its length is a multiple of 8
+    while len(packed_bits_str) % 8 != 0:
+        packed_bits_str += '0'
+    # Reverse the order to ensure the bits are packed correctly
+    print(f"Padded packed bits: {packed_bits_str}")
+    # packed_bits_str = packed_bits_str[::-1]
     # Split into chunks of 8 bits
     hex_output = []
-    for i in range(0, len(packed_bits_str), 8):
-        byte = packed_bits_str[i:i+8]
-        hex_output.append(f"0x{int(byte, 2):02x}, ")
+    while packed_bits_str:
+        # Take the last 8 bits
+        bits = packed_bits_str[-8:]
+        print(bits)
+        packed_bits_str = packed_bits_str[:-8]
+        # Convert to integer and then to hex
+        hex_output.append(f"0x{int(bits, 2):02x}, ")
+    # for i in range(len(packed_bits_str), 0, -8):
+    #     byte = packed_bits_str[i:i-8]
+    #     hex_output.append(f"0x{int(byte, 2):02x}, ")
     return ''.join(hex_output)[:-2]
 
 if __name__ == "__main__":
@@ -38,6 +50,9 @@ if __name__ == "__main__":
         sys.exit(1)
 
     input_string = sys.argv[1]
+    if len(input_string) > 634:
+        print("Error: Input string is too long. Maximum length is 634 characters.")
+        sys.exit(1)
     try:
         output = convert_to_6bit_packed_string(input_string.upper())
         print(output)
